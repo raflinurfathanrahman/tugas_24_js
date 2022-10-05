cabb0726e4ff53d480f08c49e3132ce49582eb73
raflirahman <raflirahman375@gmail.com>
2022-10-05 13:43:46 +0700

    add tugas 24

---
+++ tugas24.js
@@ -0,0 +1,10 @@
+function tampung (){
+    const angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    hasil = angka.join(',')
+    console.log('Sebelumnya :',hasil);
+    var data = angka.sort()
+    console.log('Ascending :',angka.join(','));
+    var result = data.reverse();
+    console.log('Descending :',result.join());
+}
+tampung()
